aed90bf2f42a3ab3e7296054f86f1b5a6b4b950f
nick rodriguez <nick@catalytic.com>
2020-05-18 22:17:06 -0500

    add additional linux variations

--- Formula/catalytic-cli.rb
+++ Formula/catalytic-cli.rb
@@ -6,10 +6,21 @@ class CatalyticCli < Formula
 
   if OS.mac?
     url "https://github.com/catalyticlabs/homebrew-catalytic/releases/download/v1.0.0-cli/catalytic_1.0.0_osx-x64.tar.gz"
-    sha256 "572c9fc3343a33c5d7b1ff4656d94bd155e5a256397dabe29a1485a0735afb0d"
+    sha256 "ab73a8a9fb074ae34fd7d9dd9f9fc790a14be8fc22b04760699dc07d380bc9a5"
   elsif OS.linux?
-    url "https://github.com/catalyticlabs/homebrew-catalytic/releases/download/v1.0.0-cli/catalytic_1.0.0_linux-x64.tar.gz"
-    sha256 "dce14bae7c9e8b5d5a30108d53417da11b5cf0ef392979d5fd27deb65e9b517b"
+    if Hardware::CPU.intel?
+      url "https://github.com/catalyticlabs/homebrew-catalytic/releases/download/v1.0.0-cli/catalytic_1.0.0_linux-x64.tar.gz"
+      sha256 "fc6324bd3abba00ff757d5ab411c663c0ce62ff5ce82223c60504ff9c04cf2c8"
+    end
+    if Hardware::CPU.arm?
+      if Hardware::CPU.is_64_bit?
+        url "https://github.com/catalyticlabs/homebrew-catalytic/releases/download/v1.0.0-cli/catalytic_1.0.0_linux-arm64.tar.gz"
+        sha256 "ccb7e283e720c74e141d9ff027bb83b2df7c33e5177360d56666f0e010d4852b"
+      else
+        url "https://github.com/catalyticlabs/homebrew-catalytic/releases/download/v1.0.0-cli/catalytic_1.0.0_linux-arm.tar.gz"
+        sha256 "2da6807dc286a67d9226526feaa1a6f385554a8482d6d48c3814b63ad1fd0253"
+      end
+    end
   end
 
   def install
